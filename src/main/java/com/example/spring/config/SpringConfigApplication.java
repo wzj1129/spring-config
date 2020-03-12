380b55f5f32a384d29d1da00f9e5697209626014
emil <5536065+wang_zi_jun@user.noreply.gitee.com>
2020-03-12 13:43:42 +0800

    Initial commit

---
+++ src/main/java/com/example/spring/config/SpringConfigApplication.java
@@ -0,0 +1,12 @@
+package com.example.spring.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringConfigApplication.class, args);
+    }
+}
